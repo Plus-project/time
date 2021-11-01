df69816352fbe53c59279b4cd7c200d77bef8138
金城瑠依 <rui@rui.local>
2021-11-01 21:04:40 +0900

    up to date

--- lib/Alarm/Alarm.dart
+++ lib/Alarm/Alarm.dart
@@ -6,23 +6,34 @@ class AlarmPage extends StatefulWidget {
   _AlarmPageState createState() => _AlarmPageState();
 }
 
-List<String> alarmList = [];
-
 class _AlarmPageState extends State<AlarmPage> {
+  List<String> alarmList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
           itemCount: alarmList.length,
           itemBuilder: (BuildContext context, int index) {
-            return Card(
-              child: ListTile(
-                title: Text(
-                  alarmList[index],
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.lightBlue,
-                  ),
+            return Dismissible(
+              key: Key(alarmList[index]),
+              onDismissed: (direction) {
+                setState(() {
+                  alarmList.removeAt(index);
+                });
+              },
+              background: Container(
+                alignment: Alignment.centerRight,
+                color: Colors.red,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                  child: Icon(Icons.delete_outline),
+                ),
+              ),
+              child: Card(
+                child: ListTile(
+                  title: Text(alarmList[index],
+                      style: TextStyle(fontSize: 30, color: Colors.lightBlue)),
                 ),
               ),
             );
